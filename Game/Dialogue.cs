10f62fa7a6b91522b42a8d387be084e993d60100
Pay-ton <001224922@mohawkcollege.ca>
2026-03-31 19:23:22 -0400

    load font

---
+++ Game/Dialogue.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Numerics;
+using System.Threading;
+
+public class PlayerCharacter
+{
+    string[] PlayerText =
+        [
+        "",
+        "",
+
+
+        ];
+
+}
+
+public class Narration
+{
+    string[] NarrationText =
+        [
+        "Welcome to 'The Wrath of Raph'.",
+        ""
+
+
+        ];
+
+}
+
+public class gameSystem
+{
+    string[] SystemText =
+        [
+        "You've encountered an Error",
+        "You've avoided an Error",
+
+
+        ];
+
+}
+
+public class Raph
+{
+    string[] BossRaphText =
+        [
+            "Ahhh, come to stop my minions of darkness?",
+            "Foolish, FOOLISH! No mere mortal is capable of withstanding my Wrath.",
+            "The great shapeshifting master of evil",
+        ];
+
+
+}
---
+++ Game/TextBox.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Numerics;
+using System.Threading;
+using MohawkGame2D;
+using System.Security.Cryptography.X509Certificates;
+
+
+
+public class TextBox
+{
+    
+    public Font BitCount;
+
+    public void initialize()
+    {
+        BitCount = Text.LoadFont("..\\..\\..\\Game\\Fonts\\Bitcount - Regular.ttf");
+    }
+
+    public void Write(string text)
+    {
+        Text.Font = BitCount;
+    }
+}
+
