8c03b44403dea4050fa99ccff15a17a42edef008
anaha <anaharisohn@gmail.com>
2026-04-06 20:44:32 -0400

    Added the start of the boss die

--- Game/Ai/Enemys/Boss.cs
+++ Game/Ai/Enemys/Boss.cs
@@ -10,21 +10,35 @@ public class Boss : BaseEnemy
     float FireBallDamage = 10f;
     float ProSpeed = 400f;
     float[] FireAngle = [-70f, -56, -42, -28, -14, 0, 14, 28, 42, 56, 70f];
+    bool Die1 = true;
+    bool Die2 = true;
 
     public override void CustomSetup()
     {
         base.CustomSetup();
         BodyTextureLocation = "../../../Assets/Textures/Boss.png";
+        MaxHP = 200;
+        HP = 200;
     }
 
     public override void TakeDamage(float Damage, Vector2 HitForce)
     {
         base.TakeDamage(Damage, HitForce);
-
+        if (HP <= MaxHP * .75f)
+        {
+            if (HP <= MaxHP * .25f)
+            {
+                Phase = 2;
+                return;
+            }
+            Phase = 1;
+        }
     }
 
     public override void Render()
     {
+        DamageResistance = float.Clamp(DamageResistance, 0.2f, 1f);
+
         base.Render();
         switch (Phase)
         {
@@ -58,6 +72,8 @@ public class Boss : BaseEnemy
                 Direction = Vector2.Normalize(Target.Position - Position);
                 FireBallRate = .8f;
 
+                if(Die1) { GetGame.StartDialogue(GetGame.GetDialoguePersonally.BossRaphText[11], 5f); Die1 = false; }
+
                 if (IsTimerDone(3))
                 {
                     SetTimer(3, FireBallRate);
@@ -77,6 +93,32 @@ public class Boss : BaseEnemy
                     }
                 }
 
+                break;
+
+            case 2:
+                Target = GetClosetPlayer();
+
+                if (Die1) { GetGame.StartDialogue(GetGame.GetDialoguePersonally.BossRaphText[13], 5f); Die2 = false; }
+
+
+                if (Target != null)
+                {
+                    Direction = Target.Position - Position;
+                    Move(Direction);
+                }
+
+                if (Target != null && Vector2.Distance(Target.Position, Position) <= AttackRange)
+                {
+                    Target.Velocity -= Target.Velocity * TargetSlowdown * Time.DeltaTime;
+                    Velocity -= (Velocity * TargetSlowdown * 1.4f) * Time.DeltaTime;
+                    if (IsTimerDone(4))
+                    {
+                        SetTimer(4, AttackCooldown);
+                        DealDamage(Target, AttackDamage, Vector2.Zero);
+                    }
+                }
+                else
+                    SetTimer(4, AttackCooldown);
                 break;
         }
 
--- Game/Rooms/Special/BossRoom.cs
+++ Game/Rooms/Special/BossRoom.cs
@@ -14,25 +14,7 @@ public class BossRoom : BaseRoom
         Doors = new BaseDoor[4];
         IsBossRoom = true;
 
-        //AddDoor(0,
-        //    new Vector2(Window.Width, Window.Height / 2),
-        //    new Vector2(100, Window.Height / 2),
-        //    new Vector2(1f, 0f));
-
-        //AddDoor(1,
-        //    new Vector2(0, Window.Height / 2),
-        //    new Vector2(Window.Width - 100, Window.Height / 2),
-        //    new Vector2(-1f, 0f));
-
-        //AddDoor(2,
-        //    new Vector2(Window.Width / 2, 0),
-        //    new Vector2(Window.Width / 2, Window.Height - 100),
-        //    new Vector2(0f, -1f));
-
-        //AddDoor(3,
-        //    new Vector2(Window.Width / 2, Window.Height),
-        //    new Vector2(Window.Width / 2, 100),
-        //    new Vector2(0f, 1f));
+        GetGame.StartDialogue(GetGame.GetDialoguePersonally.BossRaphText[8], 5f);
 
         GetGame.SpawnBoss();
         GetGame.AddEnemy(new Cultist(), GridPosition, CultistSpawn[0]);
